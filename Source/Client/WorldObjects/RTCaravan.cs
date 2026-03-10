d52206a63dd16417ba1141ae2b3e9c95087121c1
SirWilliam <93367389+SirrWilliam@users.noreply.github.com>
2026-03-10 20:23:33 +0300

    Add

--- Source/Client/WorldObjects/RTCaravan.cs
+++ Source/Client/WorldObjects/RTCaravan.cs
@@ -6,6 +6,29 @@ namespace GameClient.WorldObjects
 {
     public class RTCaravan : WorldObject
     {
+        public static readonly Color RTOrange = new Color(0.913f, 0.471f, 0.059f);
+
+        public override Color ExpandingIconColor => RTOrange;
+
+        private Material cachedMaterial;
+
+        public override Material Material
+        {
+            get
+            {
+                if (cachedMaterial == null)
+                {
+                    cachedMaterial = MaterialPool.MatFrom(new MaterialRequest
+                    {
+                        mainTex = (Texture2D)def.Material.mainTexture,
+                        shader = def.Material.shader,
+                        color = RTOrange
+                    });
+                }
+                return cachedMaterial;
+            }
+        }
+
         public override float DrawAltitude
         {
             get
